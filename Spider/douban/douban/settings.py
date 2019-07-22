caedb513e04efafb298754c1964beacf5e8e43a4
bingshuizhilian <bingshuizhilian@yeah.net>
2019-07-22 19:24:12 +0800

    update

--- Spider/douban/douban/settings.py
+++ Spider/douban/douban/settings.py
@@ -66,9 +66,9 @@ ROBOTSTXT_OBEY = True
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 # 注意数据库配置
-ITEM_PIPELINES = {
-   'douban.pipelines.DoubanPipeline': 300,
-}
+# ITEM_PIPELINES = {
+#    'douban.pipelines.DoubanPipeline': 300,
+# }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
