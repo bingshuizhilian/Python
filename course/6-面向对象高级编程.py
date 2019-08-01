b744db86ed88b9a1ac60509fcdddb7590616e9a2
bingshuizhilian <bingshuizhilian@yeah.net>
2019-08-01 14:25:11 +0800

    Update 6-面向对象高级编程.py

--- course/6-面向对象高级编程.py
+++ course/6-面向对象高级编程.py
@@ -384,7 +384,103 @@ ORM全称“Object Relational Mapping”，即对象-关系映射，就是把关
 这样，写代码更简单，不用直接操作SQL语句。要编写一个ORM框架，所有的类都只能动态定义，因为只有使用者才能根据表的结构定
 义出对应的类来。
 '''
-# 编写底层模块的第一步，先把调用接口写出来。
+# 定义Field类，它负责保存数据库表的字段名和字段类型
+class Field(object):
+    def __init__(self, name, column_type):
+        self.name = name
+        self.column_type = column_type
 
+    def __str__(self):
+        return '<%s:%s>' % (self.__class__.__name__, self.name)
 
+# 在Field的基础上，进一步定义各种类型的Field，比如StringField，IntegerField等
+class StringField(Field):
+    def __init__(self, name):
+        super(StringField, self).__init__(name, 'varchar(255)')
+
+class IntegerField(Field):
+    def __init__(self, name):
+        super(IntegerField, self).__init__(name, 'bigint')
+
+# 接下来是最复杂的ModelMetaclass
+class ModelMetaclass(type):
+    def __new__(cls, name, bases, attrs):
+        if 'Model' == name:
+            return type.__new__(cls, name, bases, attrs)
+
+        print('Found model: %s' % name)
+
+        mappings = dict()
+        for k, v in attrs.items():
+            if isinstance(v, Field):
+                print('Found mapping: %s => %s' % (k, v))
+                mappings[k] = v
+
+        for k in mappings.keys():
+            attrs.pop(k)
+
+        attrs['__mappings__'] = mappings  # 保存属性和列的映射关系
+        attrs['__table__'] = name  # 假设表名和类名一致
+
+        return type.__new__(cls, name, bases, attrs)
+
+# 基类Model
+class Model(dict, metaclass = ModelMetaclass):
+    def __init__(self, **kw):
+        super(Model, self).__init__(**kw)
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Model' object has no attribute '%s'" % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def save(self):
+        fields = []
+        params = []
+        args = []
+        for k, v in self.__mappings__.items():
+            fields.append(v.name)
+            params.append('?')
+            args.append(getattr(self, k, None))
+        sql = 'insert into %s (%s) values (%s)' % (self.__table__, ','.join(fields), ','.join(params))
+        print('SQL: %s' % sql)
+        print('ARGS: %s' % str(args))
+
+    def query(self):
+        pass
+
+# 编写底层模块的第一步，先写调用接口，但由于编译的关系，需要把这步放到下面
+'''
+其中，父类Model和属性类型StringField、IntegerField是由ORM框架提供的，剩下的魔术方法比如query()全部由metaclass自动
+完成。虽然metaclass的编写会比较复杂，但ORM的使用者用起来却异常简单。
+'''
+class movie_top250(Model):
+    # 定义类的属性到列的映射
+    title = StringField('title')
+    movieInfo = StringField('movieInfo')
+    star = StringField('star')
+    evaluation = StringField('evaluation')
+    quote = StringField('quote')
+    unusedItem = IntegerField('unusedItem')
+
+m = movie_top250(star = '9.4')
+m.query()
+m.save()
+
+'''
+1.当用户定义一个class User(Model)时，Python解释器首先在当前类User的定义中查找metaclass，如果没有找到，
+  就继续在父类Model中查找metaclass，找到了，就使用Model中定义的metaclass的ModelMetaclass来创建User类，
+  也就是说，metaclass可以隐式地继承到子类，但子类自己却感觉不到。
+2.在ModelMetaclass中，一共做了几件事情：
+(1)排除掉对Model类的修改；
+(2)在当前类（比如User）中查找定义的类的所有属性，如果找到一个Field属性，就把它保存到一个__mappings__
+   的dict中，同时从类属性中删除该Field属性，否则，容易造成运行时错误（实例的属性会遮盖类的同名属性）；
+(3)把表名保存到__table__中，这里简化为表名默认为类名。
+3.在Model类中，可以定义各种操作数据库的方法，比如save()，delete()，find()，update等。
+4.我们实现了save()方法，把一个实例保存到数据库中。因为有表名，属性到字段的映射和属性值的集合，就可以构造出INSERT语句。
+'''
 print('#'*10, '7.使用元类', 'start' if 0 else 'end', '#'*10)
