5a855ef1a4da294c6fa4d63ffd51eda64ac65ea2
bingshuizhilian <bingshuizhilian@yeah.net>
2019-08-01 14:53:40 +0800

    Create 7-错误、调试和测试.py

---
+++ course/7-错误、调试和测试.py
@@ -0,0 +1,20 @@
+﻿#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+' course name：错误、调试和测试 '
+
+__author__ = 'bingshuizhilian'
+
+
+
+### 1.错误处理
+
+print('#'*10, '1.错误处理', 'start' if 1 else 'end', '#'*10)
+
+
+
+
+print('#'*10, '1.错误处理', 'start' if 0 else 'end', '#'*10)
+
+
+
