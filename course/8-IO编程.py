3b33eba2e194822805558c5f9b24b8375462d3c8
bingshuizhilian <bingshuizhilian@yeah.net>
2019-08-15 10:13:48 +0800

    Create 8-IO编程.py

---
+++ course/8-IO编程.py
@@ -0,0 +1,17 @@
+﻿#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+' course name：IO编程 '
+
+__author__ = 'bingshuizhilian'
+
+
+
+### 1.文件读写
+print('#'*10, '1.文件读写', 'start' if 1 else 'end', '#'*10)
+
+
+print('#'*10, '1.文件读写', 'start' if 0 else 'end', '#'*10)
+
+
+
