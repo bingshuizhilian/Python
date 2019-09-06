86a4a095a295e0eead045170054b9ef86d50dd17
bingshuizhilian <bingshuizhilian@yeah.net>
2019-09-06 11:11:16 +0800

    Update 7.fileprocess.py

--- exercise/7.fileprocess.py
+++ exercise/7.fileprocess.py
@@ -15,13 +15,24 @@ FACTOR_STRING = ' * 9 / 10\n'
 def ModifyWAVlengthForT1EProject(srcfile, makebkpfile = True):
     srcfilebkp = srcfile + '.orig'
     lines = []
+    isalreadycatched = False
 
-    if True == makebkpfile: shutil.copy(srcfile, srcfilebkp)
     with open(srcfile, 'r', encoding='utf-8') as f: lines = f.readlines()
+
     for i in range(len(lines)):
         if lines[i].find('WAV_') >= 0 and lines[i].find('_LEN') >= 0:
+            if lines[i].find(FACTOR_STRING) >= 0:
+                isalreadycatched = True
+                break
             lines[i] = lines[i][:-1] + FACTOR_STRING
-    with open(srcfile, 'w', encoding='utf-8') as f: f.writelines(lines)
+    
+    if False == isalreadycatched:
+        if True == makebkpfile:
+            shutil.copy(srcfile, srcfilebkp)
+        with open(srcfile, 'w', encoding='utf-8') as f: f.writelines(lines)
+        print('已处理')
+    else:
+        print('未处理，因为待修改文件已经包含系数({})了'.format(FACTOR_STRING.strip()))
 
 if __name__ == "__main__":
     ModifyWAVlengthForT1EProject(SRC_FILE)
