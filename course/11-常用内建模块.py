e5c1668b1b72854a9c2477a9fd0ba06a5bd766a9
bingshuizhilian <bingshuizhilian@yeah.net>
2019-11-21 16:44:42 +0800

    Update 11-常用内建模块.py

--- course/11-常用内建模块.py
+++ course/11-常用内建模块.py
@@ -960,7 +960,7 @@ def parseXml(xml_str):
         ]
     }
 
-URL2 = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20%3D%202151330&format=xml'
+URL2 = r'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20%3D%202151330&format=xml'
 URL = 'https://www.baidu.com'
 with request.urlopen(URL, timeout=4) as f:
     data = f.read()
@@ -973,16 +973,40 @@ print('#'*10, '10.XML', 'start' if 0 else 'end', '#'*10)
 
 ### 11.HTMLParser
 '''
+利用HTMLParser，可以把网页中的文本、图像等解析出来。
 
+HTML本质上是XML的子集，但是HTML的语法没有XML那么严格，所以不能用标准的DOM或SAX来解析HTML。
+好在Python提供了HTMLParser来非常方便地解析HTML，只需简单几行代码
 '''
 print('#'*10, '11.HTMLParser', 'start' if 1 else 'end', '#'*10)
+from html.parser import HTMLParser
+from html.entities import name2codepoint
 
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print('<%s>' % tag)
+    def handle_endtag(self, tag):
+        print('</%s>' % tag)
+    def handle_startendtag(self, tag, attrs):
+        print('<%s/>' % tag)
+    def handle_data(self, data):
+        print(data)
+    def handle_comment(self, data):
+        print('<!--', data, '-->')
+    def handle_entityref(self, name):
+        print('<&%s;>' % name)
+    def handle_charref(self, name):
+        print('<&#%s;>' % name)
 
+myhtmlparser = MyHTMLParser()
+# feed()方法可以多次调用，也就是不一定一次把整个HTML字符串都塞进去，可以一部分一部分塞进去。
+# 特殊字符有两种，一种是英文表示的&nbsp;，一种是数字表示的&#1234;，这两种字符都可以通过Parser解析出来。
+myhtmlparser.feed('''<html>
+<head></head>
+<body>
+<!-- test html parser -->
+    <p>Some <a href=\"#\">html</a> HTML&nbsp;tutorial...<br>END</p>
+</body></html>'''
+)
 
 print('#'*10, '11.HTMLParser', 'start' if 0 else 'end', '#'*10)
-
-
-
-
-
-
